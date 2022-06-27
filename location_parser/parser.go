5a660c7e4483988aa55ad7bf5c12e3c6836f9132
Klim K <kirklimushin@gmail.com>
2022-06-28 00:44:24 +0300

    Added Models For Orders, Added Some Initial Integrations, Added Location Parsers, refactoring

--- location_parser/parser.go
+++ location_parser/parser.go
@@ -1,21 +1,27 @@
-package main 
+package main
 
 import (
-	"errors"
-	"os"
-	"net/http"
-	"fmt"
-	"logging"
 	"RealTLocation/location_parser/exceptions"
+	"RealTLocation/models/models"
+	"errors"
+	"fmt"
 	"json"
-	"sync"
+	"logging"
+	"models"
+	"net/http"
+	"os"
 	"reflect"
+	"strconv"
+	"sync"
+	"json"
+	"xml"
 	"github.com/go-gocron/gocron"
 )
 
 var (
 	ErrorLogger *logging.GetLogger 
 	DebugLogger *logging.GetLogger 
+	AllStates = []string{"Delivered", "Preparing", "On-the-Way"}
 )
 
 
@@ -52,14 +58,16 @@ type ParsedLocation struct {
 
 
 func (this *ParsedLocation) convertToXml() (string, error){
+	return xml.Marshal(this)
 }
 
 func (this *ParsedLocation) convertToJson() (string, error){
+	return json.Marshal(this)
 }
 
 
-
-func parse_customer_location(deviceInfo *DeviceInfo) (map[string]string, error){
+func parse_customer_location(deviceInfo *DeviceInfo) (map[string]interface{}, error){
+	return 
 }
 
 
@@ -100,6 +108,61 @@ func ParseLocationTask(deviceInfo *DeviceInfo) (interface{}){
 	}
 }
 
+type OrderState struct {
+	order_state string 
+}
+func (this *OrderState) validate_order_state() (bool){	
+	for {
+		state := AllStates
+		if this.order_state == state[0]{
+			return true 
+		}
+	};
+	return false
+}
+
+type EmailMessage struct {
+	message string 
+	receiverEmail string 
+	senderEmail string 
+}
+func (this *EmailMessage) sendEmail(){
+
+}
+var (
+	Order models.Order // represents an order Model.
+)
+
+func NotifyEmailStateUpdated(channel chan map[string]string){
+	notificationData := <- channel
+	order_id, error := notificationData["orderId"]
+	customerEmail, error := notificationData["customerEmail"]
+	state := notificationData["state"]
+	orderName, error := models.database.Model(&Order).Where("id = ?", order_id).First().order_name
+	if error != true{
+		DebugLogger.Println(fmt.Sprintf(
+		"Failed to obtain the order with ID: %s", order_id))
+	}
+	message := fmt.Printf("Hello, %s, Great News! Your Order %s has status of %s Order. Check you profile for more details.",
+	customerEmail, orderName, state)
+	// some logic of sending email.... goes here....
+}
+
+func UpgradeOrderState(orderStateChannel chan map[string]interface{}, transactionData map[string]interface{}, OrderId int, State OrderState, customerEmail string){
+	// Updates the Order State in the database and sends 
+	validatedState := State.validate_order_state()
+	sql_command := fmt.Sprintf("BEGIN TRANSACTION LOCK %s ON SHARE RULE")
+	DebugLogger.Prinln("Order with ID: %s has been updated to %s", OrderId, State)
+	database_engine := ""
+	if executed, error := database_engine.execute(sql_command); error != nil{
+		ErrorLogger.Println(
+		fmt.Sprintf("Failed to Update Order State, %s, Order ID: %s", validatedState, OrderId))
+	}
+	notificationData := map[string]interface{"state": validatedState, "orderId": orderId,
+    "customerEmail": customerEmail}
+	orderStateChannel <- notificationData
+}
+
 
 func start_consuming(deviceInfo *DeviceInfo) (bool, interface{}){
 
@@ -117,7 +180,6 @@ func start_consuming(deviceInfo *DeviceInfo) (bool, interface{}){
 		return false, error
 	}
 }
-
 
 
 
